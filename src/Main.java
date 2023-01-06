2dda02745f0ea59e340ea92585b0001f15ff259b
Nikita <33672291+khilioti@users.noreply.github.com>
2023-01-06 10:27:22 +0200

    start learning

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,101 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//  Learn1_1
+//        for (int i = 1; i <= 5; i++) {
+//           System.out.print(i + " ");
+//        }
+
+//  Learn1_2
+//        for (int i = 5; i >= 1; i--) {
+//            System.out.print(i + " ");
+//        }
+
+//  Learn1_3
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println("3*" + i + "=" + 3*i);
+//        }
+
+//  Learn1_4
+//        System.out.println("Введите целое число");
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+//        int sum = 0;
+//        for (int i = 0; i <= n; i++) {
+//            sum += i;
+//        }
+//        System.out.println(sum);
+
+// Learn2_1
+//        int num = 7;
+////        for (int i = 1; i <= 14; i++) {
+////            System.out.print(num * i + " ");
+////        }
+//        while (num <= 98) {
+//            System.out.print(num + " ");
+//            num +=7;
+//        }
+
+// Learn2_2
+//        int i = 1;
+//        while (i <= 512) {
+//            System.out.println("i = " + i);
+//            i*=2;
+//        }
+
+// Learn3_1
+//        Scanner sk = new Scanner(System.in);
+//        System.out.println("Введите количество строк");
+//        int r = sk.nextInt();
+//        System.out.println("Введите количество столбцов");
+//        int c = sk.nextInt();
+//        for (int i = 1; i <= c; i++) {
+//            for (int j = 1; j <= r; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+// Learn4_1
+//        Scanner sk = new Scanner(System.in);
+//        System.out.println("Введите количество чисел Фибоначи");
+//        int cou = sk.nextInt();
+//        int n0 = 1;
+//        int n1 = 1;
+//        int n2;
+//        System.out.print(n0 + " " + n1 + " ");
+//        for (int i = 3; i <= cou; i++) {
+//            n2 = n0 + n1;
+//            System.out.print(n2 + " ");
+//            n0 = n1;
+//            n1 = n2;
+//        }
+
+// Learn5_1
+//        String answer;
+//        boolean exit;
+//        Scanner inputAnswer = new Scanner(System.in);
+//        System.out.println("Что это такое: синий, большой, с усами и полностью набит зайцами?");
+//        exit = false;
+//        for (int i = 1; i <= 3 ; i++) {
+//            if (exit) break;
+//
+//            answer = inputAnswer.next();
+//            switch (answer){
+//                case ("Тролейбус"):
+//                    System.out.println("Правильно!");
+//                    exit = true;
+//                    break;
+//                case ("Сдаюсь"):
+//                System.out.println("Правильный ответ: троллейбус.");
+//                exit = true;
+//                break;
+//                default:
+//                System.out.println("Подумай еще.");
+//            }
+//        }
+
     }
 }
